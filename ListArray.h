463a6665a054ab9c24c80e0796b19c7134da61c9
LREMGAR <lremgar@etsinf.upv.es>
2025-10-25 16:24:28 +0200

    Cambio 2

--- ListArray.h
+++ ListArray.h
@@ -1,17 +1,30 @@
+#ifndef LISTARRAY_H
+#define LISTARRAY_H
+
 #include <ostream>
+#include <stdexcept>
 #include "List.h"
+
 
 template <typename T>
 class ListArray : public List<T> {
 
 	private:
-	       	//miembros privados
-	       	T* arr;
+	    //miembros privados
+	    T* arr;
 		int max;
  		int n;
-		static const int MINSIZE;
+		static const int MINSIZE = 2;
 		
-		void resize(int new_size);		
+		void resize(int new_size){
+			T* new_arr = new T[new_size];
+    		for (int i = 0; i < n; i++) {
+    			new_arr[i] = arr[i];
+    		}
+			delete[] arr;
+   			arr = new_arr;
+    		max = new_size;
+		}		
 
 	
 	public:
@@ -27,21 +40,86 @@ class ListArray : public List<T> {
 		}
 
 		T operator[](int pos){
-			if(pos < 0 || pos >= n)
+			if(pos < 0 || pos >= n){
 				throw std::out_of_range("Indice fuera de rango");
+			}
 			return arr[pos];
 		}
 
 		friend std::ostream& operator<<(std::ostream &out, const ListArray<T> &list) {
-     		   	for (int i = 0; i < list.n; ++i) {
+     		   	for (int i = 0; i < list.n; i++) {
             			out << list.arr[i] << " ";
         		}
         		return out;
     		}
+		
 
 		//Metodos heredados de List<T>
+		void insert(int pos, T e) override{
+			if (pos < 0 || pos > n){
+        		throw std::out_of_range("Posición inválida");
+			}
+    			if (n == max){
+        		resize(max * 2);
+			}
+    			for (int i = n; i > pos; i--) {
+       			arr[i] = arr[i - 1];
+    			}
 
+    			arr[pos] = e;
+    			n++;	
+		}
+
+
+		void append(T e) override{
+			insert(n,e);
+		}
+
+		void prepend(T e) override{
+			insert(0,e);
+		}
+
+    		void remove(int pos) override{
+			if (pos < 0 || pos >= n){
+				throw std::out_of_range("Posicion invalida");
+			}
+			for (int i = pos; i < n - 1; i++){
+				arr[i] = arr[i +1];
+			}
+			
+			n--;
+			if(n < max / 4 && max / 2 >= MINSIZE){
+				resize(max / 2);
+			}
+
+		}
+
+		int search(T e) override{
+			for (int i = 0; i < n; i++){
+				if (arr[i] == e){
+					return i;
+				}
+			}
+			return -1;
+		}
+
+		bool empty() override{
+			return n == 0;
+		}
+    	
+		int size() const override { 
+			return n; 
+		}
+    	
+		T get(int pos) const override{
+			if (pos < 0 || pos >= n){
+				throw std::out_of_range("Indice fuera de rango");
+			}
+			return arr[pos];
+		}
 
 
 
 };
+
+#endif
