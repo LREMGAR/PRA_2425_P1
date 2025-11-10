1758905d01c2011e3e96dd81d3eddd176a4b57f1
LREMGAR <lremgar@etsinf.upv.es>
2025-11-10 08:26:20 +0100

    Añadido 5

--- Node.h
+++ Node.h
@@ -1,8 +1,21 @@
-#include <ostream>
+#ifndef NODE_H
+#define NODE_H
 
+#include <ostream>
 template <typename T> 
 class Node {
     public:
         // miembros públicos
+        T data;
+        Node<T>* next;
+
+        Node(T data, Node<T>* next = nullptr) : data(data), next(next) {}
+
+        friend std::ostream& operator<<(std::ostream& out, const Node<T>& node) {
+            out << node.data;
+            return out;
+        }
     
 };
+
+#endif
